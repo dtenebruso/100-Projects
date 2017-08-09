ba2965f9a87d4356f332d3a4410b23d333e6ad03
dtenebruso <dtenebruso92@gmail.com>
2017-08-09 12:54:20 -0400

    modified argparse for zipc leading 0; write JSON response to file added

--- cur_weather.py
+++ cur_weather.py
@@ -8,16 +8,23 @@ cap = '&APPID='
 
 def zip_country_code(zipc, country, appid):
 	path = '{}{},{}{}{}'.format(base, zipc, country, cap, appid)
-	print(path)
+	print('\n\nRequest ', path,' has been sent\n\n')
 	connection = http.client.HTTPConnection('api.openweathermap.org')
 	connection.request('GET', path)
 	rawreply = connection.getresponse().read()
+
 	reply = json.loads(rawreply.decode('utf-8'))
-	print('Today will be mainly',reply['weather']['main'],'\n', reply['name'][0],'is looking at a humidity of',reply['main']['humidity'])
+
+#	Uncomment to capture the json response and write to local file
+#	with open('reply.txt', 'wb') as f:	
+#		f.write(rawreply)
+#		f.close()
+
+	print('Today will be mainly',reply['weather'][0]['main'],'\n'+reply['name']+' is looking at a humidity of',reply['main']['humidity'])
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Find your local weather')
-	parser.add_argument('zipcode', metavar='zipc', type=int, help='your local zip code(14533)')
+	parser.add_argument('zipcode', metavar='zipc', help='your local zip code(14533)')
 	parser.add_argument('countrycode', metavar='country', default='us', help='your country acronym (ex. us, uk or ru)')
 	parser.add_argument('apikey', metavar='appid', help='your api.openweathermap.org api key')
 
